ffaf6d99bdfd76a6756746851737ede186a8b143
Jordan Ketring <jordanketring@airbears2-10-142-148-125.airbears2.1918.berkeley.edu>
2018-03-07 10:23:53 -0800

    Update connection.js file to link Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,11 @@
-var mysql = require("mysql");
-
 var connection;
-
 if (process.env.JAWSDB_URL) {
+    // Heroku deployment
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
+    // local host
     connection = mysql.createConnection({
-        port: 3306,
+        port: 3000,
         host: "localhost",
         user: "root",
         password: "titus",
@@ -14,7 +13,7 @@ if (process.env.JAWSDB_URL) {
     });
 };
 
-
+// Make connection.
 connection.connect(function(err) {
     if (err) {
         console.error("error connecting: " + err.stack);
@@ -22,6 +21,3 @@ connection.connect(function(err) {
     }
     console.log("connected as id " + connection.threadId);
 });
-
-
-module.exports = connection;
